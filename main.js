cb9c21acb1d4f67b30b278f5289eeccaa6ab7537
github username <github email address>
2019-09-25 13:25:13 -0400

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.getElementById('modal')
+modal.setAttribute('class', 'hidden')
+
+const likeHearts = document.querySelectorAll('.like')
 
 
+
+function liker(event) {
+  event.preventDefault()
+  let heartIcon = event.target
+  mimicServerCall()
+    .then( serverMessage => {
+      if (heartIcon.innerText === EMPTY_HEART){ 
+      heartIcon.innerText = FULL_HEART
+      heartIcon.setAttribute('class', 'activated-heart')
+      } else {
+        heartIcon.innerText = EMPTY_HEART
+        heartIcon.removeAttribute('class')
+      }
+      alert("The server was notified")
+      alert(serverMessage)
+    })
+    .catch(error => {
+      alert("Something went wrong.")
+    })
+}
+for (let gly of likeHearts) {
+  gly.addEventListener('click', liker)
+  
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
