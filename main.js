4eafd3b9122f3611d35e5e5e0fc9442bdf9df439
github username <github email address>
2019-09-26 09:15:43 -0400

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("#modal")
+const errorModalText = document.querySelector("#modal-message")
 
-// Your JavaScript code goes here!
+function hideModal() {
+  errorModal.className = "hidden"
+}
 
+
+function likeToggle(event) {
+  const currentLikeBtn = this
+  if (this.textContent == EMPTY_HEART) {
+    mimicServerCall()
+      .then(likeSuccess => liked(currentLikeBtn))
+      .catch(error => {
+        errorModal.className = ""
+        errorModalText.textContent = error
+        setTimeout(hideModal, 5000)
+      })
+
+      function liked(likeBtn) {
+        likeBtn.textContent = FULL_HEART
+        likeBtn.className = "activated-heart"
+      }
+  } else {
+      mimicServerCall()
+        .then(unlikeSuccess => unliked(currentLikeBtn))
+        .catch(error => {
+          errorModal.className = ""
+          errorModalText.textContent = error
+          setTimeout(hideModal, 5000)
+        })
+  
+        function unliked(likeBtn) {
+          likeBtn.textContent = EMPTY_HEART
+          likeBtn.className = ""
+        }
+      
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const likeSpans = document.querySelectorAll(".like-glyph") 
+  likeSpans.forEach(likeButton => likeButton.addEventListener("click", likeToggle))
+});
 
 
 
