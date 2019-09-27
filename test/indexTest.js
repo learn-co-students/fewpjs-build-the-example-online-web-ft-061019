3be76eabe86fc8770ca57f555a172800d34916f7
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-27 11:15:35 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,9 +6,9 @@ const spies = require( 'chai-spies' );
 chai.use( spies );
 
 // Tech Team couldn't figure out why this wasn't passing and said that I did set my modal to hidden
-// describe( "main.js", () => {
-//   it("contains a hidden modal", () => {
-//     let modal = document.querySelector('.hidden')
-//     expect(modal).not.to.equal(null)
-//   } )
-// } )
+describe( "main.js", () => {
+  it("contains a hidden modal", () => {
+    let modal = document.querySelector('.hidden')
+    expect(modal).not.to.equal(null)
+  } )
+} )
