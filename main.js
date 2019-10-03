62b7c7cd1547859f5c5566932cb19b80c642c5ef
Enoch2k2 <enoch2k2@hotmail.com>
2019-10-03 08:03:25 -0500

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,77 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function() {
+  attachLikeClick();
+})
+
+//------------------------------------------------------------------------------
+// DOM Grabbers
+//------------------------------------------------------------------------------
+const getErrorModal = () => document.getElementById('modal')
+const getErrorMessage = () => document.getElementById('modal-message')
 
 
 
+
+//------------------------------------------------------------------------------
+// DOM Events
+//------------------------------------------------------------------------------
+const clickLike = e => {
+  const li = e.target;
+  mimicServerCall()
+  .then(promise => modifyHeart(li))
+  .catch(errors => {
+    showError();
+    errorMessage(errors);
+    setTimeout(hideError, 5000);
+  })
+}
+
+
+
+
+
+//------------------------------------------------------------------------------
+// DOM Event Handlers
+//------------------------------------------------------------------------------
+const attachLikeClick = () => {
+  let likes = document.querySelectorAll('.like');
+  for(let i = 0; i < likes.length; i++) {
+    likes[i].addEventListener('click', clickLike);
+  }
+}
+
+
+
+
+//------------------------------------------------------------------------------
+// DOM Manipulators
+//------------------------------------------------------------------------------
+
+const hideError = () => getErrorModal().classList.add('hidden');
+const showError = () => getErrorModal().classList.remove('hidden');
+const errorMessage = message => getErrorMessage().textContent = message;
+
+const displayFullHeart = span => {
+  span.classList.add('activated-heart')
+  span.textContent = FULL_HEART;
+}
+
+const displayEmptyHeart = span => {
+  span.classList.remove('activated-heart')
+  span.textContent = FULL_HEART;
+}
+
+const modifyHeart = li => {
+  hideError();
+  let span = li.querySelector('span');
+  if(span.classList.length < 2) {
+    displayFullHeart(span);
+  } else {
+    displayEmptyHeart(span);
+  }
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
